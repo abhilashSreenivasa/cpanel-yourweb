e45e3700cdab74415dcb1fb531fc45ce1bb5169b
abhilash sreenivasa <abhilash.sreenivasa@gmail.com>
2022-03-24 15:37:32 -0700

    added route and created stats page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express= require('express')
 const acctSummary = require('../api-data/acctSummary')
+const acctCounts=require('../api-data/acctCounts')
 const listAccounts=require('../api-data/listAccounts')
 const router=express.Router()
-
+router.get('/', async(req,res)=>{
+    const listData=await acctCounts();
+    //console.log(listData)
+    res.json(listData)
+   //res.render('home',{acctCounts:listData})
+})
 
 
 router.get('/listaccts', async(req,res)=>{
